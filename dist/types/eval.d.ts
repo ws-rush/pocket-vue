ca6493fe9bb86859c0e39450207a5f1897ec14af
ws-rush <wusaby.rush@zohomail.com>
2025-09-26 17:43:38 +0300

    fix: types generation

--- dist/types/eval.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const evaluate: (scope: object, exp: string, el: Element) => any;
-export declare const execute: (scope: any, exp: string, el?: Node) => any;
--- dist/types/scheduler.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const nextTick: (fn?: () => void) => Promise<unknown>;
-export declare const queueJob: (job: Function) => void;
