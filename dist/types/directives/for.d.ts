362c9dccce24c88ab89d6d65332f4e5a57ff3a18
ws-rush <wusaby.rush@zohomail.com>
2025-11-20 22:48:56 +0300

    release: package

--- dist/types/directives/for.d.ts
+++ dist/types/directives/for.d.ts
@@ -1,3 +1,2 @@
 import { Context } from '../context';
-export type KeyToIndexMap = Map<any, number>;
 export declare const _for: (el: Element, exp: string, ctx: Context) => ChildNode | null | undefined;
